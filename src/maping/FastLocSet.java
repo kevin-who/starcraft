4801bf1095f6200eb0bbc6f0c5e209803d3a65de
一个笨蛋 <jinchao2000@gmail.com>
2017-01-12 16:04:49 -0500

    Maps Stuff

---
+++ src/maping/FastLocSet.java
@@ -0,0 +1,41 @@
+package maping;
+
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+
+public class FastLocSet {
+	private static final int HASH = Math.max(GameConstants.MAP_MAX_WIDTH, GameConstants.MAP_MAX_HEIGHT);
+	private int size = 0;
+	private boolean[][] has = new boolean[HASH][HASH];
+	
+	public int getSize(){
+		return size;
+	}
+	
+	public void add(MapLocation loc) {
+		int x = ((int) loc.x) % HASH;
+		int y = ((int) loc.y) % HASH;
+		if (!has[x][y]) {
+			size++;
+			has[x][y] = true;
+		}
+	}
+
+	public void remove(MapLocation loc) {
+		int x = ((int) loc.x) % HASH;
+		int y = ((int) loc.y) % HASH;
+		if (has[x][y]) {
+			size--;
+			has[x][y] = false;
+		}
+	}
+
+	public boolean contains(MapLocation loc) {
+		return has[((int) loc.x) % HASH][((int) loc.y) % HASH];
+	}
+
+	public void clear() {
+		has = new boolean[HASH][HASH];
+		size = 0;
+	}
+}
