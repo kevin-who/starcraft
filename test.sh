77b21c8dfddc5e1d07330147015ba7512b56eb36
Kevin Hu <kevinhuwest@gmail.com>
2017-01-13 18:11:58 -0500

    no example

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew run -PteamA=cheddar -PteamB=mapping -Pmaps=MagicWood,LineofFire,Shrine
+./gradlew run -PteamA=cheddar -PteamB=Rusher -Pmaps=MagicWood,LineofFire,Shrine
