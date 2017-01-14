e642c7d4766605738b131d3b37bfe544cb5e70bd
一个笨蛋 <jinchao2000@gmail.com>
2017-01-13 19:16:20 -0500

    Bug semi working

--- src/stars/Bug.java
+++ src/stars/Bug.java
@@ -13,9 +13,10 @@ public class Bug {
 	private static int closestDistWhileBugging = Integer.MAX_VALUE;
 	public static MapLocation here;
 	public static RobotController rc;
+	private static Direction lastDir = null;
 
 	static boolean tryMove(RobotController rc, Direction dir) throws GameActionException {
-		return tryMove(rc, dir, 10,4);
+		return tryMove(rc, dir, 10, 4);
 	}
 
 	static boolean tryMove(RobotController rc, Direction dir, float degreeOffset, int checksPerSide)
@@ -51,6 +52,7 @@ public class Bug {
 
 	public static void goTo(MapLocation theDest) throws GameActionException {
 		if (!theDest.equals(dest)) {
+			lastDir = here.directionTo(theDest);
 			dest = theDest;
 			tracing = false;
 		}
@@ -58,6 +60,7 @@ public class Bug {
 		if (!tracing) {
 			rc.setIndicatorDot(here.add(here.directionTo(dest)), 128, 0, 0);
 			if (tryMove(rc, here.directionTo(dest))) {
+				lastDir = here.directionTo(theDest);
 				return;
 			} else {
 				startTracing();
@@ -65,6 +68,7 @@ public class Bug {
 		} else if (here.distanceSquaredTo(dest) < closestDistWhileBugging) {
 			rc.setIndicatorDot(here.add(here.directionTo(dest)), 0, 128, 0);
 			if (tryMove(rc, here.directionTo(dest))) {
+				lastDir = here.directionTo(theDest);
 				tracing = false;
 				return;
 			}
@@ -82,15 +86,31 @@ public class Bug {
 	static void traceMove() throws GameActionException {
 		Direction tryDir = here.directionTo(lastWall);
 		rc.setIndicatorDot(lastWall, 0, 128, 128);
-		
+		rc.setIndicatorLine(here, here.add(lastDir), 255, 255, 0);
+		double dif = tryDir.radians - lastDir.radians;
+		if ((dif + 3.14159265358979323846264338)
+				% (2 * 3.14159265358979323846264338) - 3.14159265358979323846264338 < 0) {
+			for (int i = 0; i < 12; i++) {
+				tryDir = tryDir.rotateLeftDegrees(30);
+				rc.setIndicatorLine(here, here.add(tryDir), 0, 0, 128);
+				if (tryMove(rc, tryDir)) {
+					lastDir = tryDir;
+					return;
+				} else {
+					lastWall = here.add(tryDir);
+				}
+			}
+		} else {
+			for (int i = 0; i < 12; i++) {
+				tryDir = tryDir.rotateRightDegrees(30);
+				rc.setIndicatorLine(here, here.add(tryDir), 0, 0, 128);
+				if (tryMove(rc, tryDir)) {
+					lastDir = tryDir;
+					return;
+				} else {
+					lastWall = here.add(tryDir);
+				}
 
-		for (int i = 0; i < 10; i++) {
-			Direction temp = tryDir.rotateRightDegrees((90 * (i/2) * (2 * (i % 2) - 1)));
-			rc.setIndicatorLine(here, here.add(temp), 0, 0, 128);
-			if (tryMove(rc, temp)) {
-				return;
-			} else {
-				lastWall = here.add(tryDir);
 			}
 		}
 	}
